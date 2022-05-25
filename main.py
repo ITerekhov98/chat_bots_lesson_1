61a4da1e1d3a3809be932719e7cbbde68e63bca6
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-05-25 16:09:42 +0300

    Cleanup the code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import time
 import requests
 import telegram
 from environs import Env
@@ -7,14 +8,12 @@ from requests.exceptions import ReadTimeout, ConnectionError
 from bot_answer_templates import title, fail_status, success_status
 
 
-def fetch_list_checks(devman_token, timestamp):
+def fetch_checks(devman_token, timestamp):
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': f'Token {devman_token}',
     }
-    params = {}
-    if timestamp:
-        params['timestamp'] = timestamp
+    params = {'timestamp': timestamp}
     try:
         response = requests.get(url, headers=headers, params=params)
     except (ReadTimeout, ConnectionError):
@@ -23,26 +22,30 @@ def fetch_list_checks(devman_token, timestamp):
     return response.json()
 
 
-def pooling_devman_api(func, devman_token, tg_chat_id, bot, timestamp=None):
+def pooling_devman_api(devman_token, tg_chat_id, bot, timestamp=None):
     while True:
-        check_detail = func(devman_token, timestamp)
-        if check_detail:
-            if check_detail['status'] == 'found':
-                timestamp = check_detail.get('last_attempt_timestamp')
-                lesson_title = check_detail['new_attempts'][0]['lesson_title']
-                lesson_url = check_detail['new_attempts'][0]['lesson_url']
+        check_detail = fetch_checks(devman_token, timestamp)
+        if  not check_detail:
+            continue
 
-                message = f"{title} '{lesson_title}' \r\n\r\n"
-                if bool(check_detail['new_attempts'][0]['is_negative']):
-                    message += fail_status
-                else:
-                    message += success_status
-                message += f"\r\n {lesson_url}"
-                bot.send_message(
-                    text=message,
-                    chat_id=tg_chat_id)
-            else:
-                timestamp = check_detail.get('timestamp_to_request')
+        if not check_detail['status'] == 'found':
+            timestamp = check_detail.get('timestamp_to_request')
+            continue
+
+        timestamp = check_detail.get('last_attempt_timestamp')
+        lesson_title = check_detail['new_attempts'][0]['lesson_title']
+        lesson_url = check_detail['new_attempts'][0]['lesson_url']
+
+        message = f"{title} '{lesson_title}' \r\n\r\n"
+        if check_detail['new_attempts'][0]['is_negative']:
+            message += fail_status
+        else:
+            message += success_status
+        message += f"\r\n {lesson_url}"
+        bot.send_message(
+            text=message,
+            chat_id=tg_chat_id)
+
 
 
 def main():
@@ -52,7 +55,6 @@ def main():
     devman_token = env.str('DEVMAN_API_TOKEN')
     tg_chat_id = env.str('TG_CHAT_ID')
     pooling_devman_api(
-        func=fetch_list_checks,
         devman_token=devman_token,
         tg_chat_id=tg_chat_id,
         bot=bot,
